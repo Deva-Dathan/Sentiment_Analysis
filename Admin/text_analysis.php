d33656e7f6c1e3f50c312f8f3ae142f3372246ad
Deva Dathan <devadathan21@gmail.com>
2023-11-16 18:35:42 +0530

    code

--- Admin/text_analysis.php
+++ Admin/text_analysis.php
@@ -473,15 +473,15 @@ nav .profile-details i{
     </div>
       <ul class="nav-links">
         <li>
-          <a href="admin_dash.php" class="active">
-            <i class='bx bx-grid-alt' style="color:#fff; font-weight:bold;"></i>
-            <span class="links_name" style="color:#fff; font-weight:bold;">Dashboard</span>
+          <a href="admin_dash.php">
+            <i class='bx bx-grid-alt' style="font-weight:bold;"></i>
+            <span class="links_name" style="font-weight:bold;">Dashboard</span>
           </a>
         </li>
         <li>
-          <a href="text_analysis.php">
-          <i class='bx bx-font'></i>
-            <span class="links_name" style="font-weight:bold;">Text Analysis</span>
+          <a href="text_analysis.php" class="active">
+          <i class='bx bx-font' style="color:#fff; font-weight:bold;"></i>
+            <span class="links_name" style="color:#fff; font-weight:bold;">Text Analysis</span>
           </a>
         </li>
         <li>
@@ -527,52 +527,6 @@ nav .profile-details i{
     </nav>
 
     <div class="home-content">
-      <div class="overview-boxes">
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Order</div>
-            <div class="number">40,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart-alt cart'></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Sales</div>
-            <div class="number">38,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-add cart two' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Profit</div>
-            <div class="number">$12,876</div>
-            <div class="indicator">
-              <i class='bx bx-up-arrow-alt'></i>
-              <span class="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i class='bx bx-cart cart three' ></i>
-        </div>
-        <div class="box">
-          <div class="right-side">
-            <div class="box-topic">Total Return</div>
-            <div class="number">11,086</div>
-            <div class="indicator">
-              <i class='bx bx-down-arrow-alt down'></i>
-              <span class="text">Down From Today</span>
-            </div>
-          </div>
-          <i class='bx bxs-cart-download cart four' ></i>
-        </div>
-      </div>
 
       <div class="sales-boxes">
         <div class="recent-sales box">
